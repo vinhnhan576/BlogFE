7946fb2702a7988d4f434f01cdc9adef79a474ee
vnhxn <99260394+vinhnhan576@users.noreply.github.com>
2023-02-08 02:41:07 +0700

    update login ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { authenticateUserAsync } from '../features/account/accountSlice';
 import { getUserByUsernameAsync } from '../features/user/userSlice';
 import { Link, useNavigate } from 'react-router-dom';
 
-import loginImage01 from '../assets/image/login/login-image-01.png';
-import logo from '../assets/image/login/login-logo.png';
+import bannerPic1 from '../assets/image/login/banner-pic-1.png';
+import bannerPic2 from '../assets/image/login/banner-pic-2.png';
+import logo from '../assets/image/login/logo.png';
 import { unwrapResult } from '@reduxjs/toolkit';
 
 const Login = () => {
@@ -18,12 +19,21 @@ const Login = () => {
     //     console.log(user)
     //     navigate('/' + user.alias + '/')
     // }
-    const initialUserState = {
+    var initialUserState = {
         username: '',
         password: '',
     };
 
+    document.addEventListener('onautocomplete', function (e) {
+        if (e.target.hasAttribute('autocompleted'))
+            // true or false
+            console.log(e.target);
+        // e.preventDefault(); // prevent autocomplete
+    });
+
     const [account, setAccount] = useState(initialUserState);
+
+    const [blogger, setBlogger] = useState('');
 
     const handleChange = (input) => (event) => {
         setAccount({ ...account, [input]: event.target.value });
@@ -44,51 +54,213 @@ const Login = () => {
             })
         );
         const originalPromiseResult = unwrapResult(result);
-        const alias = result.payload.tasks.result.alias;	
+        const alias = result.payload.tasks.result.alias;
         navigate(`/${alias}/`);
         if (originalPromiseResult.tasks.alias !== undefined)
             navigate(`/${originalPromiseResult.tasks.alias}/`);
     };
 
+    const leftBg = useRef();
+    const logoRef = useRef();
+    const lowerRef = useRef();
+    const rightRef = useRef();
+    const backRef = useRef();
+    const onMobileLoginClick = () => {
+        leftBg.current.classList.toggle('login-clicked');
+        logoRef.current.classList.toggle('login-clicked');
+        lowerRef.current.classList.toggle('login-clicked');
+        rightRef.current.classList.toggle('login-clicked');
+        backRef.current.classList.toggle('login-clicked');
+    };
+
+    // const onMobileBackBtnClick = () => {
+    //     leftBg.current.classList.remove('login-clicked');
+    //     logoRef.current.classList.remove('login-clicked');
+    //     lowerRef.current.classList.remove('login-clicked');
+    //     rightRef.current.classList.remove('login-clicked');
+    //     backRef.current.classList.remove('login-clicked');
+    // }
+
     return (
         <div className="login">
-            <div className="login__image">
-                <img src={loginImage01} alt="" />
+            <div className="login__container">
+                <div className="login__left">
+                    {/* <img src={loginImage01} alt="" /> */}
+                    <div className="login__left__bg" ref={leftBg}>
+                        <img
+                            src={bannerPic1}
+                            alt=""
+                            className="login__left__bg__pic1"
+                        />
+                        {/* <img
+                            src={bannerPic2}
+                            alt=""
+                            className="login__left__bg__pic2"
+                        /> */}
+                        <div className="login__left__bg__message">
+                            <div className="login__left__bg__message__upper">
+                                Writing isn't just a hobby,
+                            </div>
+                            <div className="login__left__bg__message__lower">
+                                It's a{' '}
+                                <span className="login__left__bg__message__lower__highlight">
+                                    self-improvement
+                                </span>{' '}
+                                activity!
+                            </div>
+                        </div>
+                    </div>
+                    <div className="login__logo__mobile" ref={logoRef}>
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className="login__lower" ref={lowerRef}>
+                        <div className="login__lower__message">
+                            Dive into the ocean of inspiration
+                        </div>
+                        <button
+                            onClick={onMobileLoginClick}
+                            className="login__form__button login__left__login-btn"
+                        >
+                            Login
+                        </button>
+                        <div className="login__form__or login__lower__or">
+                            <div className="login__form__or__text">or</div>
+                        </div>
+                        <div className="login__form__item login__form__find">
+                            {/* <div className="login__form__item__title">
+                                tài khoản
+                            </div> */}
+                            <input
+                                type="text"
+                                name="find"
+                                id="find"
+                                autoComplete="off"
+                                aria-labelledby="placeholder-fname"
+                                value={blogger}
+                                onChange={(e) => setBlogger(e.target.value)}
+                            />
+                            <label
+                                className="login__form__item__placeholder-text"
+                                for="fname"
+                                id="placeholder-fname"
+                            >
+                                <div className="login__form__item__text">
+                                    Find a blogger
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className="login__right" ref={rightRef}>
+                    <div
+                        className="login__right__back"
+                        onClick={onMobileLoginClick}
+                        ref={backRef}
+                    >
+                        {'<'}
+                    </div>
+                    <form
+                        className="login__form"
+                        onSubmit={handleLogin}
+                        autoComplete="off"
+                    >
+                        <div className="login__logo">
+                            <img src={logo} alt="" />
+                        </div>
+                        <div className="login__welcome">Welcome!</div>
+                        <div className="login__welcome2">
+                            Enter your username and password <br /> and we're
+                            ready to go
+                        </div>
+                        <div className="login__form__item">
+                            <input
+                                type="text"
+                                name="username"
+                                id="username"
+                                autoComplete="off"
+                                aria-labelledby="placeholder-fname"
+                                value={account.username}
+                                onChange={handleChange('username')}
+                            />
+                            <label
+                                className="login__form__item__placeholder-text"
+                                for="fname"
+                                id="placeholder-fname"
+                            >
+                                <div className="login__form__item__text">
+                                    Username
+                                </div>
+                            </label>
+                        </div>
+                        <div className="login__form__item">
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                autoComplete="off"
+                                aria-labelledby="placeholder-fname"
+                                value={account.password}
+                                onChange={handleChange('password')}
+                            />
+                            <label
+                                className="login__form__item__placeholder-text"
+                                for="fname"
+                                id="placeholder-fname"
+                            >
+                                <div className="login__form__item__text">
+                                    Password
+                                </div>
+                            </label>
+                        </div>
+                        <button
+                            type="submit"
+                            className="login__form__button login__form__login-button"
+                        >
+                            Login
+                        </button>
+                        <div className="login__form__or login__right__or">
+                            <div className="login__form__or__text">or</div>
+                        </div>
+                        <div className="login__form__item login__form__find login__right__find">
+                            {/* <div className="login__form__item__title">
+                                tài khoản
+                            </div> */}
+                            <input
+                                type="text"
+                                name="find"
+                                id="find"
+                                autoComplete="off"
+                                aria-labelledby="placeholder-fname"
+                                value={blogger}
+                                onChange={(e) => setBlogger(e.target.value)}
+                            />
+                            <label
+                                className="login__form__item__placeholder-text"
+                                for="fname"
+                                id="placeholder-fname"
+                            >
+                                <div className="login__form__item__text">
+                                    Find a blogger
+                                </div>
+                            </label>
+                        </div>
+                        <div className="login__form__signup">
+                            Don't have an account yet?
+                            <Link to="signup">
+                                <span className="login__form__signup__btn">
+                                    {' '}
+                                    Sign up.
+                                </span>
+                            </Link>
+                        </div>
+                        <Link to="signup">
+                            <div className="login__form__mobile-signup">
+                                Sign up
+                            </div>
+                        </Link>
+                    </form>
+                </div>
             </div>
-            <form className="login__form" onSubmit={handleLogin}>
-                <div className="login__logo">
-                    <img src={logo} alt="" />
-                </div>
-                <div className="login__form__item">
-                    <div className="login__form__item__title">tài khoản</div>
-                    <input
-                        type="text"
-                        name="username"
-                        id="username"
-                        onChange={handleChange('username')}
-                    />
-                </div>
-                <div className="login__form__item">
-                    <div className="login__form__item__title">mật khẩu</div>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        onChange={handleChange('password')}
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="login__form__button login__form__login-button"
-                >
-                    đăng nhập
-                </button>
-                <Link to="signup">
-                    <button type="submit" className="login__form__button">
-                        đăng ký
-                    </button>
-                </Link>
-            </form>
         </div>
     );
 };
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,132 +1,139 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-import { createNewAccountAsync } from "../features/account/accountSlice";
-import { useNavigate } from "react-router-dom";
+import { createNewAccountAsync } from '../features/account/accountSlice';
+import { useNavigate } from 'react-router-dom';
 
-import loginImage01 from "../assets/image/login/login-image-01.png";
+import loginImage01 from '../assets/image/login/login-image-01.png';
 
 const Signup = () => {
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-	const initialAccountState = {
-		name: "",
-		alias: "",
-		gender: true,
-		date: '2023-01-01',
-		tel: "",
-		job: "",
-		address: "",
-		email: "",
-		profilepic: "",
-		upperpic: "",
-		lowerpic: "",
-		username: "",
-		password: "",
-	};
+    const initialAccountState = {
+        name: '',
+        alias: '',
+        gender: true,
+        date: '2023-01-01',
+        tel: '',
+        job: '',
+        address: '',
+        email: '',
+        profilepic: '',
+        upperpic: '',
+        lowerpic: '',
+        username: '',
+        password: '',
+    };
 
-	const [fullName, setFullName] = useState({ firstName: "", surName: "" });
+    const [fullName, setFullName] = useState({ firstName: '', surName: '' });
 
-	const [account, setAccount] = useState(initialAccountState);
+    const [account, setAccount] = useState(initialAccountState);
 
-	const handleChange = (input) => (event) => {
-		setAccount({ ...account, [input]: event.target.value });
-	};
+    const handleChange = (input) => (event) => {
+        setAccount({ ...account, [input]: event.target.value });
+    };
 
-	const handleNameChange = (input) => (event) => {
-		fullName[input] = event.target.value;
-		account.name = fullName.surName + " " + fullName.firstName;
-		// setAccount({
-		// 	...account,
-		// 	name: fullName.surName + " " + fullName.firstName,
-		// });
-	};
+    const handleNameChange = (input) => (event) => {
+        fullName[input] = event.target.value;
+        account.name = fullName.surName + ' ' + fullName.firstName;
+        // setAccount({
+        // 	...account,
+        // 	name: fullName.surName + " " + fullName.firstName,
+        // });
+    };
 
-	const handleSignUp = async (e) => {
-		e.preventDefault();
-		console.log("registering...");
-		setAccount({ ...account, date: new Date(account.date) });
-		await dispatch(createNewAccountAsync({ account }));
-		navigate("/");
-	};
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        console.log('registering...');
+        setAccount({ ...account, date: new Date(account.date) });
+        await dispatch(createNewAccountAsync({ account }));
+        navigate('/');
+    };
 
-	
-
-	return (
-		<div className="login">
-			<div className="login__image signup">
-				<img src={loginImage01} alt="" />
-			</div>
-			{/* <div className="login__image login__image__after signup">
+    return (
+        <div className="login">
+            <div className="login__image signup">
+                <img src={loginImage01} alt="" />
+            </div>
+            {/* <div className="login__image login__image__after signup">
 				<img src={loginImage02} alt="" />
 			</div> */}
-			<form className="login__form" onSubmit={handleSignUp}>
-				<div className="login__form__item">
-					<div className="login__form__item__whole__small__title">
-						<div className="login__form__item__small__title">
-							<div className="login__form__item__title"> Họ</div>
-							<input
-								type="text"
-								name="surName"
-								id="surName"
-								onChange={handleNameChange("surName")}
-								// placeholder={"Cẩm".toString()}
-							/>
-						</div>
-						<div className="login__form__item__small__title">
-							<div className="login__form__item__title"> Tên</div>
-							<input
-								type="text"
-								name="firstName"
-								id="firstName"
-								onChange={handleNameChange("firstName")}
-								// placeholder={"Sục".toString()}
-							/>
-						</div>
-					</div>
-				</div>
-				<div className="login__form__item">
-					<div className="login__form__item__title">Biệt danh</div>
-					<input
-						type="text"
-						name="alias"
-						id="alias"
-						onChange={handleChange("alias")}
-						// placeholder={"huongleehere".toString()}
-					/>
-				</div>
-				<div className="login__form__item">
-					<div className="login__form__item__title">Tên tài khoản</div>
-					<input
-						type="text"
-						name="username"
-						id="username-signup"
-						onChange={handleChange("username")}
-						// placeholder={"huongleehere".toString()}
-					/>
-				</div>
-				<div className="login__form__item">
-					<div className="login__form__item__title">Mật khẩu</div>
-					<input
-						type="password"
-						name="password"
-						id="password-signup"
-						onChange={handleChange("password")}
-						// placeholder={"Abc_123456".toString()}
-					/>
-				</div>
-				<div className="login__form__item">
-					<div className="login__form__item__title">Email</div>
-					<input
-						type="text"
-						name="email"
-						id="email"
-						onChange={handleChange("email")}
-						// placeholder={"Bloggit@gmail.com".toString()}
-					/>
-				</div>
-				{/* <div className="login__form__item">
+            <form className="login__form" onSubmit={handleSignUp}>
+                <div className="login__form__item">
+                    <div className="login__form__item__whole__small__title">
+                        <div className="login__form__item__small__title">
+                            <div className="login__form__item__title"> Họ</div>
+                            <input
+                                type="text"
+                                name="surName"
+                                id="surName"
+                                onChange={handleNameChange('surName')}
+                                // placeholder={"Cẩm".toString()}
+                            />
+                        </div>
+                        <div className="login__form__item__small__title">
+                            <div className="login__form__item__title"> Tên</div>
+                            <input
+                                type="text"
+                                name="firstName"
+                                id="firstName"
+                                onChange={handleNameChange('firstName')}
+                                // placeholder={"Sục".toString()}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="login__form__item">
+                    <input
+                        type="text"
+                        name="alias"
+                        id="alias"
+                        onChange={handleChange('alias')}
+						value={account.alias}
+                        // placeholder={"huongleehere".toString()}
+                    />
+                    <label
+                        className="login__form__item__placeholder-text"
+                        for="fname"
+                        id="placeholder-fname"
+                    >
+                        <div className="login__form__item__text">Alias</div>
+                    </label>
+                </div>
+                <div className="login__form__item">
+                    <div className="login__form__item__title">
+                        Tên tài khoản
+                    </div>
+                    <input
+                        type="text"
+                        name="username"
+                        id="username-signup"
+                        onChange={handleChange('username')}
+                        // placeholder={"huongleehere".toString()}
+                    />
+                </div>
+                <div className="login__form__item">
+                    <div className="login__form__item__title">Mật khẩu</div>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password-signup"
+                        onChange={handleChange('password')}
+                        // placeholder={"Abc_123456".toString()}
+                    />
+                </div>
+                <div className="login__form__item">
+                    <div className="login__form__item__title">Email</div>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        onChange={handleChange('email')}
+                        // placeholder={"Bloggit@gmail.com".toString()}
+                    />
+                </div>
+                {/* <div className="login__form__item">
 					<div className="login__form__item__title">Số điện thoại </div>
 					<input
 						type="text"
@@ -136,7 +143,7 @@ const Signup = () => {
 						placeholder={"0123456789".toString()}
 					/>
 				</div> */}
-				{/* <div className="login__form__item">
+                {/* <div className="login__form__item">
 					<div className="login__form__item__title">Ngày sinh </div>
 					<input
 						type="text"
@@ -146,7 +153,7 @@ const Signup = () => {
 						// placeholder="02/11/2002"
 					/>
 				</div> */}
-				{/* <div className="login__form__item">
+                {/* <div className="login__form__item">
 					<div className="login__form__item__title">Địa chỉ </div>
 					<input
 						type="text"
@@ -156,7 +163,7 @@ const Signup = () => {
 						placeholder={"ex: Thủy Phương, Hương Thủy".toString()}
 					/>
 				</div> */}
-				{/* <div className="login__form__item">
+                {/* <div className="login__form__item">
 					<div className="login__form__item__title"> Nghề nghiệp </div>
 					<input
 						type="text"
@@ -166,12 +173,12 @@ const Signup = () => {
 						placeholder={"Má thiên hạ".toString()}
 					/>
 				</div> */}
-				<button type="submit" className="login__form__button">
-					Đăng ký
-				</button>
-			</form>
-		</div>
-	);
+                <button type="submit" className="login__form__button">
+                    Đăng ký
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default Signup;
